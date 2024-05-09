2a9e872cf34e6932e165590300dbf9275fa8ced9
Dika1020 <dikaputraganteng@gmail.com>
2024-05-09 10:35:08 +0700

    PROJEK 1

---
+++ projek1/pukesmas-app/admin/ceklogin.php
@@ -0,0 +1,22 @@
+<?php
+if (isset($_POST['email'])) {
+    session_start();
+    require_once('dbkoneksi.php');
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $sql = "Select * FROM user where email = ? and password = md5(?)";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute([$email, $password]);
+    $row = $stmt->fetch();
+
+    if ($row) {
+        $_SESSION['login'] = true;
+        $_SESSION['nama'] = $row['nama'];
+        echo "<script>window.location.href='index.php'</script>";
+    } else {
+        echo "<script>alert('Selamat datang');window.location.href='index.php'</script>";
+    }
+}
+
+//SAYA UDH COBA PAK/BNG UDH MASUKIN GMAIL SAMA PW SUDAH SESUAI KENAPA TETAP SALAH YA
